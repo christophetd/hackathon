ff8cf6365e2c400fd1140c14261139bd9006a6c2
Hadrien Milano <hadrien.milano@gmail.com>
2013-03-23 19:34:09 +0100

    added public

---
+++ index.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(80);
+
+console.log("server running and listening on port 80");

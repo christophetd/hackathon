063b31206154ce4b24b1d1d86893fe9504ea5e1e
Manu <manu@clement-HP-Mini-110-3000.(none)>
2013-03-23 22:14:31 +0100

    Ajout de la méthode addSong à la classe Playlist

--- Server/Playlist.js
+++ Server/Playlist.js
@@ -2,5 +2,10 @@
 module.exports = function (){
 	//list : Song []
 	this.list= new Array();
-	this.
+	//addSong : void
+	//song : Song
+	//Adds a song at the end of the queue
+	this.addSong=function(song){
+		this.list[this.list.length] = song;
+	}
 }

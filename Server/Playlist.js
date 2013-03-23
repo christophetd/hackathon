f26dbf3a358c24ed9742f2a5a9bf627a03b4817f
Manu <manu@clement-HP-Mini-110-3000.(none)>
2013-03-23 22:32:01 +0100

    Ajout de la variable id_counter pour générer l'id du Song

--- Server/Playlist.js
+++ Server/Playlist.js
@@ -2,6 +2,9 @@
 module.exports = function (){
 	//list : Song []
 	this.list= new Array();
+	//private id_counter : int
+	//autoincremented, id of the next song entering in the queue
+	var id_counter = 1;
 	//public addSong() : void
 	//song : Song
 	//Adds a song at the end of the queue

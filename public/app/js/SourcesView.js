4cdfface95def656cb2039603c35a873fe7680e5
Philippe Schwaller <SchwallerP@tsf-444-wpa-0-123.epfl.ch>
2013-03-24 14:18:59 +0100

    sourcesview

--- public/app/js/SourcesView.js
+++ public/app/js/SourcesView.js
@@ -2,11 +2,33 @@
 	
 	//HTML list holding search results in the library
 	var list = $('#library_list');
-	
+	var lf = new LocalFiles();
+	var YTsearch = function(toAdd){
+		$.post('/api/'+ hash +'/search', {q: toAdd, n: 6})
+			.done(function(data) {
+			console.log(data);
+			data = JSON.parse(data);
+			console.log(data.length);
+
+	});
+
 	$('#library_search').submit(function(evt){
+		alert('submit');
 		evt.preventDefault();
-		
+		var toAdd = $("input[id=appendedInputButton]").val();
+        var localresults = lf.search(toAdd);
+        var youtuberesults = YTsearch(toAdd);
+        var results = localresults.concat(youtuberesults);
+        alert('Hello!');
+        for(var i = 0 ; i < results.length ; i++){
+				var item = results[i];
+				var liItem = $('<li id="' + item.id + '""></li>');
+				liItem.append($('<a href="#">' + item.name + '</a>'));
+				$('#library_dynamicResults').append(liItem);
+			  
+		}
 		//TODO : implement search
+		return false;
 	});
 	
 
@@ -14,4 +36,4 @@
 		//TODO : add files to localFiles
 		
 	});
-}
+};}

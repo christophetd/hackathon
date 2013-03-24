f3a21a06f7f41aafa1aa36713eb052d372043325
Manu <manu@clement-HP-Mini-110-3000.(none)>
2013-03-24 09:33:47 +0100

    added add(), remove(), get()

--- public/app/js/LocalFiles.js
+++ public/app/js/LocalFiles.js
@@ -1,8 +1,30 @@
 //class LocalFiles
 module.export = function(socket){
 	this.list=[];
-
-	this.search(words){
+	//add() : void
+	//path : string
+	//adds the path into the list, avoiding doublons
+	this.add=function(path){
+		for(var p in list)
+			if (p==path)
+				return;
+		this.list.push(path);
+	}
+	//remove() : void
+	//path : string
+	//removes the path from the list
+	this.remove=function(path){
+		for(var ind=0;ind<this.list.length;i++)
+			if (p==path){
+				this.list.splice(ind,1);
+				return;
+			}
+	}
+	//search () : string[]
+	//words : string []
+	//returns all the members of the list containing at least on of the words,
+	//sorted by pertinence
+	this.search=function(words){
 		var scores = new Array(this.list.length);
 		var results = [];
 		for(var ind=0;ind<list.length;i++)
@@ -14,5 +36,10 @@ module.export = function(socket){
 				if (scores[ind] == i)
 					results.push(list[ind]);
 		return results;
+	}
+	//get () : string []
+	//returns the whole list
+	this.get=function(){
+		return list[];
 	}
 }

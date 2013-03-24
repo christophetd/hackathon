8007576c3e8ccf7ea8fa5b071551b74dca03973a
Philippe Schwaller <SchwallerP@tsf-444-wpa-0-123.epfl.ch>
2013-03-24 01:31:36 +0100

    public/mobile/music.html

--- public/mobile/script.js
+++ public/mobile/script.js
@@ -1,15 +1,39 @@
-$(document).ready(function() {
+$(document).bind('pageinit',function() {
     $('#addnew').click(function() {
     	var toAdd = $("input[name=search-1]").val();
         $('#messages').append("<p>"+toAdd+"</p>");
     });
 
     $('#refresh-1').click(function() {
-        $('#message1').append("<p>"+"Hello world"+"</p>");
+        $.getJSON('test.json', function(data) {
+  			var items = [];
+ 			
+  			//Modify Vote-list
+
+  			items.push('<ul id="dynamicFieldList" data-role="listview" data-icon="arrow-u" data-filter="true" data-filter-placeholder="Search for music.." data-count-theme="c">');
+ 			
+  			$.each(data, function(i, item) {
+    			items.push('<li id="' + item.id + '"><a href="#">' + item.name + '<span class="ui-li-count">'+ item.score +'</span></a></li>'); 
+  			});
+
+  			items.push('</ul>');
+
+  			$('#votelist').empty().append(items.join(''));
+
+        $('#dynamicFieldList').listview();
+    });
+});
+    // Up Post Function, this.id 
+    $("#dynamicFieldList li").click(function() {
+        $.post('/api/foo/up', {id: 0})
+          .done(function(data) {
+          alert("Data Loaded: " + data);
+        });
     });
 
-    $('#refresh-2').click(function() {
-        $('#message2').append("<p>"+"Hello world"+"</p>");
-    });
+
 
 });
+
+
+

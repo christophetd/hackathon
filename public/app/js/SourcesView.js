cc6a20b72abe6fe28efa82c95c6c606d50363f5e
Philippe Schwaller <SchwallerP@tsf-444-wpa-0-123.epfl.ch>
2013-03-24 14:42:47 +0100

    Sourcesview

--- public/app/js/SourcesView.js
+++ public/app/js/SourcesView.js
@@ -5,36 +5,34 @@ function SourcesView(hash, localFiles){
 	//HTML list holding search results in the library
 	var list = $('#library_list');
 	var lf = new LocalFiles();
-	var YTsearch = function(toAdd){
+	var YTsearch = function(toAdd, callback){
 		$.post('/api/'+ hash +'/search', {q: toAdd, n: 6})
 			.done(function(data) {
-			console.log(data);
+			alert(data);
 			data = JSON.parse(data);
-			console.log(data.length);
+			callback(data)
 		});
 	};
 
 	$('#library_search').submit(function(evt){
-		alert('submit');
 		evt.preventDefault();
 
 		var toAdd = $("input[id=appendedInputButton]").val();
         var localresults = lf.search(toAdd);
-        var youtuberesults = YTsearch(toAdd);
-        var results = localresults.concat(youtuberesults);
-        alert('Hello!');
-        for(var i = 0 ; i < results.length ; i++){
-				var item = results[i];
-				var liItem = $('<li id="' + item.id + '""></li>');
-				liItem.append($('<a href="#">' + item.name + '</a>'));
-				$('#library_dynamicResults').append(liItem);
-			  
+        YTsearch(toAdd,function(youtuberesults){
+
+	        var results = localresults.concat(youtuberesults);
+	        alert("RESULTS :"+results);
+	        for(var i = 0 ; i < results.length ; i++){
+	        		alert(results[i]);
+					var item = results[i];
+					var liItem = $('<li id="' + item.id + '""></li>');
+					liItem.append($('<a href="#">' + item.name + '</a>'));
+					$('#library_dynamicResults').append(liItem);
+	        };
 		}
-		
-		console.log("hello les pauvres");
-		//TODO : implement search
-		return false;
-	});
+	)})
+	//});
 	
 	_this = this;
 	

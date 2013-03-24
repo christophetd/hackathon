f177030f02608e223124c36377acb1e3576ad4cf
Philippe Schwaller <SchwallerP@tsf-444-wpa-0-123.epfl.ch>
2013-03-24 02:30:08 +0100

    public/mobile/music.html

--- public/mobile/script.js
+++ public/mobile/script.js
@@ -1,39 +1,76 @@
 $(document).bind('pageinit',function() {
-    $('#addnew').click(function() {
-    	var toAdd = $("input[name=search-1]").val();
-        $('#messages').append("<p>"+toAdd+"</p>");
-    });
+    
 
     $('#refresh-1').click(function() {
         $.getJSON('test.json', function(data) {
-  			var items = [];
- 			
-  			//Modify Vote-list
+  			
+        $('#dynamicFieldList').empty()
+ 			  $('#dynamicFieldList').html();
+        $.each(data, function(i, item) {
+            var liItem = $('<li id="' + item.id + '"><a href="#">' + item.name + '<span class="ui-li-count">'+ item.score +'</span></a></li>');
+            //Up function
+            liItem.click(function(){
+                $.post('/api/foo/up', {id: this.id})
+                .done(function(data) {
+                alert("Data Loaded: " + data);
+                });
+                alert(item);
+            });
+            
+            liItem.append($('<a href="#">' + item.name + '</a>'));
+            $('#dynamicFieldList').append(liItem);
+          
+        });
+  			$('#dynamicFieldList').listview('refresh');
+        });
 
-  			items.push('<ul id="dynamicFieldList" data-role="listview" data-icon="arrow-u" data-filter="true" data-filter-placeholder="Search for music.." data-count-theme="c">');
- 			
-  			$.each(data, function(i, item) {
-    			items.push('<li id="' + item.id + '"><a href="#">' + item.name + '<span class="ui-li-count">'+ item.score +'</span></a></li>'); 
-  			});
 
-  			items.push('</ul>');
-
-  			$('#votelist').empty().append(items.join(''));
-
-        $('#dynamicFieldList').listview();
     });
-});
-    // Up Post Function, this.id 
-    $("#dynamicFieldList li").click(function() {
-        $.post('/api/foo/up', {id: 0})
+
+    //Search function
+    $('#addnew').click(function() {
+      var toAdd = $("input[name=search-1]").val();
+        // modify string
+        $.post('/api/foo/search', {q: toAdd})
           .done(function(data) {
           alert("Data Loaded: " + data);
         });
+
+        $.getJSON('test.json', function(data) {
+      
+          $('#dynamicResults').empty()
+        $('#dynamicResults').html();
+        $.each(data, function(i, item) {
+            var liItem = $('<li id="' + item.id + '""></li>');
+            liItem.click(function(){
+
+                $.post('/api/foo/add', {id: item})
+                .done(function(data) {
+                alert("Data Loaded: " + data);
+                });
+                alert(item);
+            });
+            //liItem.data=item;
+            liItem.append($('<a href="#">' + item.name + '</a>'));
+            $('#dynamicResults').append(liItem);
+          
+        });
+
+        $('#dynamicResults').listview('refresh');
+        });
+    
     });
 
 
 
+
+
 });
+
+$(document).ready(function(){
+  $('#dynamicResults').listview();
+  $('#dynamicFieldList').listview();
+})
 
 
 

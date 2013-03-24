7b172370cf591a3b285b670b9851724049167560
Hadrien Milano <hadrien.milano@gmail.com>
2013-03-24 14:17:11 +0100

    file addition

--- public/app/js/SourcesView.js
+++ public/app/js/SourcesView.js
@@ -1,4 +1,6 @@
-﻿function SourcesView(hash, localFiles){
+﻿var _this;
+
+function SourcesView(hash, localFiles){
 	
 	//HTML list holding search results in the library
 	var list = $('#library_list');
@@ -6,12 +8,25 @@
 	$('#library_search').submit(function(evt){
 		evt.preventDefault();
 		
+		console.log("hello les pauvres");
 		//TODO : implement search
 	});
 	
-
-	$('#library_addFilesButton').click(function(){
-		//TODO : add files to localFiles
+	_this = this;
+	
+	this.openFiles = function(files){
+		console.log(files[0]);
+		$('#library_filesInput').replaceWith(
+			$('<input style="overflow: hidden; width: 1000px; height: 1000px; opacity: 0; position: absolute;" id="library_filesInput" type="file" class="btn" accept="sound/*" multiple onchange="sourcesView.openFiles(this.files)" name="localFileSelection"/>')
+		);
 		
-	});
+		for(var i = 0 ; i < files.length ; i++){
+			localFiles.add(files[i]);
+		}
+	};
+	
+}
+
+function sourceViewOpenFiles(files){
+	_this.openFiles(files);
 }

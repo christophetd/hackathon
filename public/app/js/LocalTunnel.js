6e59175f062b6a4934b30ff3b0b6b6ba6d46509d
Manu <manu@clement-HP-Mini-110-3000.(none)>
2013-03-24 10:56:33 +0100

    just somme correction related to the previous commit on LocalFiles

--- public/app/js/LocalTunnel.js
+++ public/app/js/LocalTunnel.js
@@ -2,7 +2,6 @@ var LocalFiles = require('./LocalFiles.js');
 //class LocalTunnel
 module.exports = function(socket){
 	socket.on('source_query',function(data){
-		var terms = data.split(' ');
-		socket.emit('source_search',LocalFiles.search(terms));
+		socket.emit('source_search',LocalFiles.search(data));
 	})
 }

a4d218675e7729510ba556bf3ef05be454b251ed
Manu <manu@clement-HP-Mini-110-3000.(none)>
2013-03-24 03:30:50 +0100

    now the search method retrives songs instead of links, wich is pretty awesome

--- server/Sources.js
+++ server/Sources.js
@@ -4,26 +4,32 @@
  });
  */ 
 var http = require('http');
+var Song = require('./Song.js');
 module.exports = {
 	YoutubeSource : function(){
 		//public search () : void
 		//terms : string
 		//nb : int
-		//callback(results : string[])
+		//callback(results : Song[])
 		//gets the nb firsts results for the terms
 		this.search=function(terms,nb,callback){
 			terms=terms.replace(' ','+');
 			var url="http://gdata.youtube.com/feeds/api/videos?alt=json&q="+terms;
-			var urls=[];
+			var songs=[];
 			http.get(url, function(res) {
 			  var data='';
 			  res.on('data',function(chunk){
 			  	data+=chunk;
 			  })
 			  res.on('end',function(){
-			  	for(var i=0;i<nb;i++)
-			  		urls.push(JSON.parse(data).feed.entry[i].link[0].href);
-				callback (urls);
+			  	for(var i=0;i<nb;i++){
+			  		songs.push (new Song(
+			  			JSON.parse(data).feed.entry[i].title.$t,
+			  			'youtube',
+			  			JSON.parse(data).feed.entry[i].link[0].href
+			  		));
+			  	}
+				callback (songs);
 			  })
 			});
 		}

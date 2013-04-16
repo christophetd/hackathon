7bb8c82aa06bc7b910d1519129a0532dd763a680
Christophe Tafani-Dereeper <christophe.tafani-dereeper@epfl.ch>
2013-04-16 21:23:11 +0200

    Beginning of SearchPage view + id attribute to mainContents (including css fix)

--- www/app/js/views/SearchPage.js
+++ www/app/js/views/SearchPage.js
@@ -5,25 +5,61 @@
     handle the task of querying additionnal results if we want more (ie. scrolling down the page
     should display results until no more result can be found on any source...)
  */
-define(['jquery', 'PageFragment', 'backbone'], function($, PageFragment){
+define(['jquery', 'PageFragment', 'common/js/util/Search.js', 'app/js/util/FakeSource.js', 'backbone'], function($, PageFragment, SearchAggregator, FakeSource){
     return PageFragment.extend({
         
-        // the constructor
+        $scrollingElement: $('#mainContainer'), 
+        dataLoading: false,
+        keywords: '', 
+        CHUNK_SIZE: 20,
+
         initialize: function () {
             this.template = _.template($('#searchTemplate').html());
             
-            this.query = '';
-        },
-        
-        // Proceeds a search
-        search: function(query){
-            this.query = query;
-            this.render();
+            _.bindAll(this, 'detectPageBottom', 'bottomReached', 'loadResult');
+            this.searchAggregator = new SearchAggregator({
+                    chunkSize: this.CHUNK_SIZE, 
+                    preloadThreshold: 5
+            });
+
+            this.searchAggregator.addSrc(FakeSource);
+            this.query = this.searchAggregator.query(this.keywords);
+            this.query.exec();
+            
+
+            /* Load initial results */
+            var self = this;
+            SearchAggregator.util.fetchResults(this.query, this.CHUNK_SIZE, {
+                read: function(data) {
+                    self.loadResult(data);
+                }
+            });
+            
+            this.detectPageBottom();
         },
 
-        // populates the html to the dom
+        loadResult: function(result) {
+            this.$el.find('#results').html("kikoo");
+        },
+
+        detectPageBottom: function() {
+            this.$scrollingElement.bind('scroll', $.proxy(function() {
+                if(this.$scrollingElement.scrollTop() + this.$scrollingElement.height() >= $("#mainContents").height() && this.dataLoading === false) {
+                    this.bottomReached();
+                }
+            }, this));
+        },
+
+        bottomReached: function() {
+
+        }, 
+
+        search: function(query) {
+            this.keywords = query;
+        }, 
+
         render: function () {
-            this.$el.html(this.template({query: this.query}));
+            this.$el.html(this.template({query: this.keywords}));
             return this;
         }
     });
